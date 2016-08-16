ef1b41e177242f9459c359efe21511d832d36d84
Romain Isnel <romainisnel@gmail.com>
2016-08-16 11:58:45 +0200

    Add params on packs calls

--- index.js
+++ index.js
@@ -212,8 +212,13 @@
           });
         },
 
-        getPacks: function () {
-          return apiGet('pack?__populate=covers.imgThumb');
+        getPacks: function (skip) {
+          if 
+            (skip) {
+            return apiGet('pack?__populate=covers.imgThumb&format=full&skip=' + skip);
+          } else {
+            return apiGet('pack?__populate=covers.imgThumb&format=full');  
+          }
         },
 
         getPack: function (packId) {
